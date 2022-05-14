636fdf3c8a2df7f5398b9f066e57f1d3f7c9792f
julietafc <juli47g1@stud.kea.dk>
2022-05-14 17:59:24 +0200

    SecInfo complete

--- src/components/SecInfo/SecInfo.jsx
+++ src/components/SecInfo/SecInfo.jsx
@@ -1,3 +1,4 @@
+import Button from "../Button/Button";
 import styles from "../SecInfo/secInfo.module.scss";
 export default function SecInfo(props) {
   const img2Style = {
@@ -12,12 +13,14 @@ export default function SecInfo(props) {
   return (
     <section className={styles.SecInfo}>
       <div className={styles.images}>
-        <img src="../assets/pic02.webp" alt="" />
-        <img src="../assets/pic01.webp" alt="" />
+        <img src="../assets/im01.png" alt="" />
+        <img src="../assets/img02.png" alt="" />
+        <img src="../assets/img03.png" alt="" />
       </div>
       <div className={styles.text}>
         <h2 className={styles.h2}>Inspiring a conscious lifestyle for parents so you can focus on what matters most - your family.</h2>
         <p>Get the best out of your time with your little ones by subscribing to a curated selection of clothes, toys & gear for children aged 0-6, that cater to their growth, developmental milestones and the changing seasons.</p>
+        <Button label="subscribe now" />
       </div>
     </section>
   );
