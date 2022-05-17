905c7c3dcb70967fc583f937b159eb057c93701a
julietafc <juli47g1@stud.kea.dk>
2022-05-17 23:55:31 +0200

    values and guide structured

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,45 +1,34 @@
+import Banner from "../components/Banner/Banner";
+import Chat from "../components/Chat/Chat";
+import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 import "../sass/layout/layout.scss";
 export default function Guide() {
   return (
-    <>
+    <div className="guide_wrapper">
       <NavBar />
-      <main className="layout_innerpages">
-        <section>
-          <img src="../assets/img04.png" alt="" />
-          <div>
-            <h3>littlessentials care guide</h3>
-            <h1>Caring for your essential items</h1>
-            <p>Follow our simple guide for best care practices of your children’s clothing, toys & gear. Make your items last and use them again and again.</p>
+      <Chat />
+      <Banner className="banner_guide" heading="Caring for your essential items" text="Follow our simple guide for best care practices of your children’s clothing, toys & gear. Make your items last and use them again and again." />
+      <main className="layout_innerpages getintouch guide">
+        <aside>
+          <h6>Follow & Connect with us and our community</h6>
+          <div className="social">
+            <a href="#">Instagram</a>
+            <a href="#">Facebook</a>
           </div>
-        </section>
-        <section className="articles_wrapper">
-          <article>
-            <p>
-              <strong>Avoid perfumes in washing products & softeners </strong>
-              <p>Avoid contact with harsh perfumes in detergents. These will wear out your garments faster. Use neutral soaps or natural products such as soap berries instead. If you like fragrances, you can use a few drops of essential oils.</p>
-            </p>
-          </article>
-          <article>
-            <p>
-              <strong>Air out </strong>
-              <p>Use your clothing as long as possible, preferably naturally airing your items out before washing them. This way the fabric will last longer and you will be able to enjoy your items for a longer time. </p>
-            </p>
-          </article>
-          <article>
-            <p>
-              <strong>Store in a closed wardrobe </strong>
-              <p>Keep your items in a closed off wardrobe to avoid dust and humidity. Keep your toys in toy bags or boxes.</p>
-            </p>
-          </article>
-          <article>
-            <p>
-              <strong>Pass it on </strong>
-              <p>Pass the items your child has outgrown on to a friend, family member or to one of our local partners. Read more about our partners here.</p>
-            </p>
-          </article>
+        </aside>
+        <section>
+          <h3>Avoid perfumes in washing products & softeners </h3>
+          <p>Avoid contact with harsh perfumes in detergents. These will wear out your garments faster. Use neutral soaps or natural products such as soap berries instead. If you like fragrances, you can use a few drops of essential oils.</p>
+          <h3>Air out </h3>
+          <p>Avoid contact with harsh perfumes in detergents. These will wear out your garments faster. Use neutral soaps or natural products such as soap berries instead. If you like fragrances, you can use a few drops of essential oils.</p>
+          <h3>Store in a closed wardrobe </h3>
+          <p>Keep your items in a closed off wardrobe to avoid dust and humidity. Keep your toys in toy bags or boxes.</p>
+          <h3>Pass it on</h3>
+          <p>Pass the items your child has outgrown on to a friend, family member or to one of our local partners. Read more about our partners here.</p>
         </section>
       </main>
-    </>
+      <Footer />
+    </div>
   );
 }
--- src/pages/Values.jsx
+++ src/pages/Values.jsx
@@ -1,60 +1,40 @@
 import NavBar from "../components/NavBar/NavBar";
+import Banner from "../components/Banner/Banner";
+import Chat from "../components/Chat/Chat";
+import Footer from "../components/Footer/Footer";
 import "../sass/layout/layout.scss";
 export default function Values() {
   return (
     <>
       <NavBar />
-      <main className="layout_innerpages">
-        <section>
-          <img src="../assets/img04.png" alt="" />
-          <div>
-            <h3>Less is more, for sure.</h3>
-            <h1>Our values</h1>
-            <p>
-              We aim at simplifying life, so we can focus on what really matters. While we’re at it, we want to reduce waste and support local brands. It’s all possible, by following a few steps. The time is here to live our lives with intention,
-              kindness and care for the planet and its people.
-            </p>
+      <Chat />
+      <Banner className="banner_guide" heading="Our values" text="Follow our simple guide for best care practices of your children’s clothing, toys & gear. Make your items last and use them again and again." />
+      <main className="layout_innerpages getintouch guide">
+        <aside>
+          <h6>Follow & Connect with us and our community</h6>
+          <div className="social">
+            <a href="#">Instagram</a>
+            <a href="#">Facebook</a>
           </div>
-        </section>
-        <section className="articles_wrapper">
-          <article>
-            <p>
-              <strong>Simplicity. </strong>
-              <p>We aim to reduce clutter to have a clear mind, focusing only on the essential items needed for our children.</p>
-            </p>
-          </article>
-          <article>
-            <p>
-              <strong>Authenticity</strong>
-              <p>Being your honest, true self is a path of self-exploration. There is no end-goal, it’s all about the journey.</p>
-            </p>
-          </article>
-          <article>
-            <p>
-              <strong>Inclusive </strong>
-              <p>We celebrate our diversity as humans. No matter your gender, income level, or limitations, we are here to support you.</p>
-            </p>
-          </article>
-          <article>
-            <p>
-              <strong>Inspirational </strong>
-              <p>Parenthood is a rite of passage. We want to establish a community and platform where you can find resources, share struggles and celebrate your victories together.</p>
-            </p>
-          </article>
-          <article>
-            <p>
-              <strong>Conscious </strong>
-              <p>We want to create awareness so that we can live with intention. As we learn more, we share more.</p>
-            </p>
-          </article>
-          <article>
-            <p>
-              <strong>Kind </strong>
-              <p>We recognize that we all have our stories, struggles & learnings. We are empathetic and compassionate. Because we are all just human.</p>
-            </p>
-          </article>
+        </aside>
+        <section>
+          <h3>Simplicity </h3>
+          <p>We aim to reduce clutter to have a clear mind, focusing only on the essential items needed for our children.</p>
+          <h3>Authenticity </h3>
+          <p>Being your honest, true self is a path of self-exploration. There is no end-goal, it’s all about the journey.</p>
+          <h3>Inclusive </h3>
+          <p>We celebrate our diversity as humans. No matter your gender, income level, or limitations, we are here to support you.</p>
+          <h3>Inspirational</h3>
+          <p>Parenthood is a rite of passage. We want to establish a community and platform where you can find resources, share struggles and celebrate your victories together.</p>
+          <h3>Inspirational</h3>
+          <p>Parenthood is a rite of passage. We want to establish a community and platform where you can find resources, share struggles and celebrate your victories together.</p>
+          <h3>Conscious</h3>
+          <p>We want to create awareness so that we can live with intention. As we learn more, we share more.</p>
+          <h3>Kind</h3>
+          <p>We recognize that we all have our stories, struggles & learnings. We are empathetic and compassionate. Because we are all just human.</p>
         </section>
       </main>
+      <Footer />
     </>
   );
 }
