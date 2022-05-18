3dcaddc9626ac5cab97f34092899a3088b141f99
julietafc <juli47g1@stud.kea.dk>
2022-05-18 08:21:49 +0200

    section ig structured

--- src/components/SectionsHome/SectionIG.jsx
+++ src/components/SectionsHome/SectionIG.jsx
@@ -1,18 +1,18 @@
-import Cards from "../Cards/CardsMission";
-import "./SectionMission.scss";
+import "./SectionManifesto.scss";
+import Button from "../Button/Button";
 export default function SectionIG(props) {
   return (
-    <section className="mission">
-      <Cards
-        className="card_left"
-        heading="Our mission"
-        text="We offer high quality, timeless products for children that contribute to conscious consumption and waste reduction by partnering up with local designers & brands and addressing their surplus - benefiting your closet, wallet and the planet."
-      />
-      <Cards
-        className="card_right"
-        heading="Who is this for?"
-        text="Our subscription service curates essential items for your children in a simple & convenient way, removing a part of the overwhelm that comes with being a parent at times. We’re here to empower you, so you can simplify your life and make more time for your kids, family and yourself."
-      />
+    <section className="manifesto ig">
+      <div className="header">
+        <h2>Follow our journey</h2>
+        <Button className="button" label="check us out" />
+      </div>
+      <div className="img_wrapper">
+        <img src="../assets/img07.png" alt="" />
+        <img src="../assets/img08.png" alt="" />
+        <img src="../assets/img09.png" alt="" />
+        <img src="../assets/img10.png" alt="" />
+      </div>
     </section>
   );
 }
