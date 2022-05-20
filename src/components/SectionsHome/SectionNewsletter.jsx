3408b8c14304be712a955abb142828290e74af6c
julietafc <juli47g1@stud.kea.dk>
2022-05-20 11:25:42 +0200

    newsletter responsive

--- src/components/SectionsHome/SectionNewsletter.jsx
+++ src/components/SectionsHome/SectionNewsletter.jsx
@@ -1,5 +1,6 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import "./SectionNewsletter.scss";
 
 export default function SectionNewsletter(props) {
   return (
