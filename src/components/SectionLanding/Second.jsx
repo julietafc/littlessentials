fb4ffa8a1cd0e6bb5bbd49d0857aec2cc6567470
julietafc <juli47g1@stud.kea.dk>
2022-05-21 11:43:46 +0200

    all the elements there, missing finetune

---
+++ src/components/SectionLanding/Second.jsx
@@ -0,0 +1,10 @@
+export default function Second(props) {
+  return (
+    <section className="second">
+      <p>
+        Littlessentials wants to spark a genuine conversation around parenting and inspire connection. <br />
+        We want to empower you by sharing struggles & celebrating victories together, so that we can all get closer to eachother - and ourselves.{" "}
+      </p>
+    </section>
+  );
+}
---
+++ src/components/SectionLanding/Testimonial.jsx
@@ -0,0 +1,9 @@
+export default function Testimonial() {
+  return (
+    <section className="testimonial">
+      <p>
+        <strong>“The cards enabled me to talk to my partner about some of my deepest desires & needs as a mother. They brought us so much closer together. Thank you for creating these!”</strong> <br /> Mia Sardini, 32
+      </p>
+    </section>
+  );
+}
