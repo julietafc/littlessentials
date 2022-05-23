d6bf6da53f6e4f9d57df3db368822e5ad057d9da
julietafc <juli47g1@stud.kea.dk>
2022-05-23 14:36:02 +0200

    styling brand list and brand

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,20 +1,48 @@
 import React, { useEffect, useState } from "react";
 
+import "./Brand.scss";
+
 export default function Brand(props) {
   return (
-    <>
-      <div className="layout_innerpages brands">
-        <h1>{props.brand.title}</h1>
-        <p>{props.brand.description}</p>
-        <div className="card_wrapper"></div>
-        <button
+    <div className="wrapper">
+      <div className="brand_detail">
+        <a
+          className="return_header"
           onClick={() => {
             props.setBrand(null);
           }}
         >
-          Return to all brands{" "}
-        </button>
+          Return to all brands
+        </a>
+        <div className="brand_header">
+          <div className="header_text">
+            <h1>{props.brand.title}</h1>
+            <p>LOCATION: Copenhagen</p>
+          </div>
+          <img src="../assets/img11.png" alt="" />
+        </div>
+        <div className="brand_body">
+          <aside>
+            <h4>Website</h4>
+            <a href={props.brand.website}>{props.brand.website}</a>
+            <h4>Certifications</h4>
+            <p>website url</p>
+            <h4>Category</h4>
+            <p>Toys</p>
+          </aside>
+          <section>
+            <p>{props.brand.description}</p>
+          </section>
+        </div>
+        <a
+          className="return_footer"
+          onClick={() => {
+            props.setBrand(null);
+          }}
+        >
+          Return to all brands
+        </a>
       </div>
-    </>
+    </div>
   );
 }
