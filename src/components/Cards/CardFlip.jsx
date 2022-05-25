38d65581179cd8ceac66ceb231d6a919122c8982
julietafc <juli47g1@stud.kea.dk>
2022-05-25 16:30:49 +0200

    styling cards

--- src/components/Cards/CardFlip.jsx
+++ src/components/Cards/CardFlip.jsx
@@ -3,21 +3,22 @@ import ReactCardFlip from "react-card-flip";
 import "./Cards.scss";
 
 export default function CardFlip(props) {
+  const styleCard = { backgroundColor: props.bgColor, color: props.color };
   const [flipped, setFlipped] = useState(false);
   function handleClick() {
     setFlipped((flipped) => !flipped);
   }
   return (
     <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
-      <article className="cards">
-        <div>
+      <article style={styleCard} className="cards" onClick={handleClick}>
+        <div className="front">
           <p>{props.number}</p>
           <h4>{props.heading}</h4>
         </div>
-        <button onClick={handleClick}>+</button>
+        {/* <button onClick={handleClick}>+</button> */}
       </article>
-      <div>
-        <button onClick={handleClick}>X</button>
+      <div className="back" onClick={handleClick}>
+        {/* <button onClick={handleClick}>X</button> */}
         <p>{props.description}</p>
       </div>
     </ReactCardFlip>
