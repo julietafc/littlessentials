abe35525cb7fb65b9e5a4d6fdf0a3d8e18c5b254
Miguel <migu0403@stud.kea.dk>
2022-06-01 22:19:17 +0200

    fix layout testimonial

--- src/components/SectionsHome/SectionTestimonial.jsx
+++ src/components/SectionsHome/SectionTestimonial.jsx
@@ -1,23 +1,33 @@
+import { Col, Row } from "react-bootstrap";
 import "../../sass/layout/layout.scss";
 import "./SectionTestimonial.scss";
-
 import "../../App.scss";
+
 export default function SectionTestimonial(props) {
   return (
     <div className="wrapper">
-      <section className="testimonial">
-        <h2 className="heading_mobile"> Essential Community</h2>
-        <img src="../assets/img12.webp" alt="curly hair woman with a kid" />
-        <div className="text_wrapper">
-          <h2 className="heading_desktop"> Essential Community</h2>
-          <div className="text">
-            <p>
-              “I love the peace of mind Littlessentials brings, knowing that I’m getting high-quality products that I actually need for my daughter each month, without having to think about it. My subscription grows with her and is just so convenient
-              to have. It’s been one of the greatest services I got since becoming a parent.”
-            </p>
-            <figcaption>- Ida L., mother of 18-month old Luna</figcaption>
-          </div>
-        </div>
+      <section className="testimonial col">
+        {/* <h2 className="heading_mobile"> Essential Community</h2> */}
+        <Row className="d-md-none d-sm-inline w-100 mb-3">
+          <h2 className="text-center "> Essential Community</h2>
+        </Row>
+        <Row>
+          <Col className="d-flex justify-content-center justify-content-md-end pe-lg-5 col-12 col-md ">
+            <img src="../assets/img12.webp" alt="curly hair woman with a kid" />
+          </Col>
+          <Col>
+            <div className="text_wrapper d-flex justify-content-center mt-4 d-md-block">
+              <h2 className="heading_desktop"> Essential Community</h2>
+              <div className="text">
+                <p>
+                  “I love the peace of mind Littlessentials brings, knowing that I’m getting high-quality products that I actually need for my daughter each month, without having to think about it. My subscription grows with her and is just so
+                  convenient to have. It’s been one of the greatest services I got since becoming a parent.”
+                </p>
+                <figcaption>- Ida L., mother of 18-month old Luna</figcaption>
+              </div>
+            </div>
+          </Col>
+        </Row>
       </section>
     </div>
   );
