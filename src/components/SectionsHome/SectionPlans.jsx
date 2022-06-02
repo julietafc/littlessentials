0f7a5ae4a63ba3b2dc104437d6b3a77233a844f4
Miguel <migu0403@stud.kea.dk>
2022-06-02 09:02:10 +0200

    style section plans

--- src/components/SectionsHome/SectionPlans.jsx
+++ src/components/SectionsHome/SectionPlans.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Plan from "../Plan/Plan";
 import { plans } from "../../modules/options";
+import "./SectionPlans.scss";
 
 export default function SectionPlans() {
   const plansList = plans.map((plan, i) => <Plan key={`plan-${i + 1}`} plan={plan} />);
 
   return (
     <div className="wrapper">
-      <Container className="mb-5">
-        <Row className="justify-content-center gy-4">{plansList}</Row>
-      </Container>
+      <section className="plans">
+        <div className="header">
+          <h2>Our Plans</h2>
+        </div>
+        <Container className="mb-5">
+          <Row className="justify-content-center gy-4">{plansList}</Row>
+        </Container>
+      </section>
     </div>
   );
 }
