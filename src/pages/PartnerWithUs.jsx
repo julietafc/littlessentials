f535e5f2b3f5c9c2c8fd935206a4f42f2568f489
Yolene Iracema Acke <iracema.acke@gmail.com>
2022-06-08 12:01:38 +0200

    alt and images

--- src/pages/PartnerWithUs.jsx
+++ src/pages/PartnerWithUs.jsx
@@ -12,16 +12,27 @@ export default function PartnerWithUs() {
     <>
       <Header />
       <Chat />
-      <Banner heading="Partner With Us" text="We can help your business by connecting you to conscious costumers" bgColor={"#8b5028"} color={"#d1cbbe"} />
+      <Banner
+        heading="Partner With Us"
+        text="We can help your business by connecting you to conscious customers"
+        bgColor={"#8b5028"}
+        color={"#d1cbbe"}
+      />
       <div className="layout_innerpages getintouch">
         <aside>
           <div>
             <h6>Follow & Connect with us and our community</h6>
             <div className="social">
-              <a href="https://www.instagram.com/littlessentialsuniverse/?hl=es" target="_blank">
+              <a
+                href="https://www.instagram.com/littlessentialsuniverse/?hl=es"
+                target="_blank"
+              >
                 Instagram
               </a>
-              <a href="https://www.facebook.com/littlessentialsuniverse" target="_blank">
+              <a
+                href="https://www.facebook.com/littlessentialsuniverse"
+                target="_blank"
+              >
                 Facebook
               </a>
             </div>
@@ -30,7 +41,8 @@ export default function PartnerWithUs() {
         <section>
           <p>
             We’d love to hear from you with any feedback or comments. <br />
-            We also encourage you to write to our amazing support team through the chat
+            We also encourage you to write to our amazing support team through
+            the chat
           </p>
           <Form>
             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -40,15 +52,24 @@ export default function PartnerWithUs() {
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Your message</Form.Label>
-              <Form.Control as="textarea" placeholder="Type here whatever comes up in your mind" style={{ height: "100px" }} />
+              <Form.Control
+                as="textarea"
+                placeholder="Type here whatever comes up in your mind"
+                style={{ height: "100px" }}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="I've read the Terms & Conditions" />
+              <Form.Check
+                type="checkbox"
+                label="I've read the Terms & Conditions"
+              />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -9,38 +9,69 @@ export default function Story() {
     <div className="guide_wrapper">
       <Header />
       <Chat />
-      <Banner heading="Our story" text="On a mission to provide parents with organic children's clothing, sustainable toys & gear, so they can focus on what really matters." bgColor={"#f0f0f0"} color={"#8b5028"} />
+      <Banner
+        heading="Our story"
+        text="On a mission to provide parents with organic children's clothing, sustainable toys & gear, so they can focus on what really matters."
+        bgColor={"#f0f0f0"}
+        color={"#8b5028"}
+      />
       <div className="layout_innerpages">
         <aside>
           <div>
             <h6>Follow & Connect with us and our community</h6>
             <div className="social">
-              <a href="https://www.instagram.com/littlessentialsuniverse/?hl=es" target="_blank">
+              <a
+                title="link to Littlessentials Instagram page"
+                href="https://www.instagram.com/littlessentialsuniverse/?hl=es"
+                target="_blank"
+              >
                 Instagram
               </a>
-              <a href="https://www.facebook.com/littlessentialsuniverse" target="_blank">
+              <a
+                title="link to Littlessentials Facebook page"
+                href="https://www.facebook.com/littlessentialsuniverse"
+                target="_blank"
+              >
                 Facebook
               </a>
             </div>
           </div>
-          <img className="el" src="../../assets/color_eco.svg" alt="moon shapes tan color" />
+          <img
+            className="el"
+            src="../../assets/color_bubbles.svg"
+            alt="Littlessentials illustration of color bubbles"
+          />
         </aside>
         <section>
           <h3>Our mission </h3>
           <p>
-            We offer high quality, timeless products for children that contribute to conscious consumption and waste reduction by partnering up with local designers & brands and addressing their surplus - benefiting your closet, wallet and the
-            planet.
+            We offer high quality, timeless products for children that
+            contribute to conscious consumption and waste reduction by
+            partnering up with local designers & brands and addressing their
+            surplus - benefiting your closet, wallet and the planet.
           </p>
           <h3>Who are we? </h3>
           <p>
-            Littlessentials was created by three Copenhagen-based designers, out of a shared value for sustainability and desire to make a difference in the world. As parents and parents-to-be of the future generation, we have come to understand the
-            amount of potential waste generated with having kids. On average, children will use about 280 pieces of clothing in just the first two years of their life. Because they grow so fast, most of these are only worn for 2-3 months, and are
-            discarded while still being in good condition. This creates two issues; parents constantly need to purchase new items for their children and a huge amount of unnecessary waste is generated.
+            Littlessentials was created by three Copenhagen-based designers, out
+            of a shared value for sustainability and desire to make a difference
+            in the world. As parents and parents-to-be of the future generation,
+            we have come to understand the amount of potential waste generated
+            with having kids. On average, children will use about 280 pieces of
+            clothing in just the first two years of their life. Because they
+            grow so fast, most of these are only worn for 2-3 months, and are
+            discarded while still being in good condition. This creates two
+            issues; parents constantly need to purchase new items for their
+            children and a huge amount of unnecessary waste is generated.
           </p>
           <p>
-            To combat these problems, Littlessentials curates the most essential items of clothing, toys and other gear necessary for children aged 0-6, so that parents can enjoy their time with those that matter most, their family. We have partnered
-            up with local brands that offer high-quality, timeless products and work with their overstock in order to reduce waste. When your children have grown out of their items, you can pass them on to our NGO partners, so that the items aren't
-            discarded, but can be enjoyed by others in your community.
+            To combat these problems, Littlessentials curates the most essential
+            items of clothing, toys and other gear necessary for children aged
+            0-6, so that parents can enjoy their time with those that matter
+            most, their family. We have partnered up with local brands that
+            offer high-quality, timeless products and work with their overstock
+            in order to reduce waste. When your children have grown out of their
+            items, you can pass them on to our NGO partners, so that the items
+            aren't discarded, but can be enjoyed by others in your community.
           </p>
         </section>
       </div>
