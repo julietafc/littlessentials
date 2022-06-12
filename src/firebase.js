3c76d194f3843cca7d12c7985962dd88212e14ce
julietafc <juli47g1@stud.kea.dk>
2022-06-12 18:22:57 +0200

    deleting commented code

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,3 @@
-// const hola = process.env.REACT_APP_FIREBASE_API_KEY;
-// console.log("hola", hola);
-// import firebase from "firebase/app";
-// import "dotenv/config";
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
@@ -15,15 +10,7 @@ const app = initializeApp({
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-  // apiKey: "AIzaSyAl5GqF8RNOPYzwzfm_QI3z1fVhHk5fcxE",
-  // authDomain: "littlessentials-dev.firebaseapp.com",
-  // projectId: "littlessentials-dev",
-  // storageBucket: "littlessentials-dev.appspot.com",
-  // messagingSenderId: "839683234168",
-  // appId: "1:839683234168:web:bdeec696637f4a30b1108d",
-  // measurementId: "G-Z39KDM5EC6",
 });
 
-// export const auth = app.auth();
 export default app;
 export const auth = getAuth(app);
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.scss";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthProvider } from "./contexts/AuthContext";
 import { ButtonsStateProvider } from "./contexts/ButtonsStateContext";
 import { SubscriptionProvider } from "./contexts/SubscriptionContext";
